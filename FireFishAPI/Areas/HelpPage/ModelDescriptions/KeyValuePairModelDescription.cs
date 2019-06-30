7af13e35e4b8c0b9f6c293d175ef0d346da114b8
Nos <noscere1978@gmail.com>
2019-06-30 14:54:00 +0100

    Removal of unneeded (default) files from project

--- FireFishAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace FireFishAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- FireFishAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FireFishAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
